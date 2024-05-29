8a5fc63d96e82c990c1bbadf1059c9ff1f824a17
fankayii <wendy.ke.1225@gmail.com>
2024-05-28 22:03:25 -0700

    add code

---
+++ Assets/Script/LaserPtr.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Unity;
+public class LaserPtr: MonoBehaviour
+{
+    public float _laserLength;
+    private LineRenderer _lineRenderer;
+
+    void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        Vector3 endPos = transform.position + (transform.right * _laserLength);
+        _lineRenderer.SetPositions(new Vector3[] {transform.position,endPos });
+    }
+}
